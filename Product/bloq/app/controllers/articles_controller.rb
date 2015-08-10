1435b60c4d134669fd011667297ec53113e423ff
ronaldo <ronald_ocampo@ultimatesoftware.com>
2015-08-10 10:31:48 -0400

    ULTI-180293: Ron - Removing basic authentication

--- Product/bloq/app/controllers/articles_controller.rb
+++ Product/bloq/app/controllers/articles_controller.rb
@@ -1,6 +1,4 @@
 class ArticlesController < ApplicationController
-  http_basic_authenticate_with name: "admin", password: "secret", except: [:index, :show]
-
   def index
     @articles = Article.all
   end
