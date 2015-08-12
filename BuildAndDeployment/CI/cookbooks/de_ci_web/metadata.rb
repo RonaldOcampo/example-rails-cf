95df3065bb4d23cdffa43d8f9cdf088aeef3d514
nicolec <nicole_carey@ultimatesoftware.com>
2015-08-12 11:58:22 -0400

    ULTI-180294 - Adding initial cookbook

---
+++ BuildAndDeployment/CI/cookbooks/de_ci_web/metadata.rb
@@ -0,0 +1,5 @@
+name             'de_ci_web'
+maintainer       'Ultimate Software'
+license          'All rights reserved'
+description      'Installs/Configures de_ci_web'
+version          '0.1.0'
---
+++ BuildAndDeployment/CI/cookbooks/de_ci_web/recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: de_ci_web
+# Recipe:: default
+#
+# Copyright 2015, Ultimate Software
+#
+# All rights reserved - Do Not Redistribute
+#
