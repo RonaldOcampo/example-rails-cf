091178fa05dc1fb021c2ef248ee4c5ec26b9ed8a
ronaldo <ronald_ocampo@ultimatesoftware.com>
2015-09-04 10:16:03 -0400

    ULTI-182630: Ron - Updating socket for base haproxy check

--- BuildAndDeployment/CI/cookbooks/de_ci_lb/metadata.rb
+++ BuildAndDeployment/CI/cookbooks/de_ci_lb/metadata.rb
@@ -1,7 +1,7 @@
 name             'de_ci_lb'
 maintainer       'Ultimate Software'
 description      'Installs/Configures de_ci_lb'
-version          '0.1.0'
+version          '0.1.1'
 
 depends          'spade', '~> 0.3.9'
 depends          'hardening'
